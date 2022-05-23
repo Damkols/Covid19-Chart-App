4b5b4229cc78dece84ef2b15b9f808e6fb11b26a
damkols <damolausmankolapo@gmail.com>
2022-05-23 12:29:49 -0800

    chore: created and worked on the Global covid information component

---
+++ src/components/GlobalInfo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface GlobalInfoProps {
+  newConfirmed: number;
+  newDeaths: number;
+  newRecovered: number;
+}
+
+const GlobalInfo: React.FunctionComponent<GlobalInfoProps> = ({
+  newConfirmed,
+  newDeaths,
+  newRecovered,
+}) => {
+  return (
+    <div>
+      <h1>Global Covid-19 data</h1>
+      <h3>New Confirmed: {new Intl.NumberFormat().format(newConfirmed)}</h3>
+      <h3>New Deaths: {new Intl.NumberFormat().format(newDeaths)}</h3>
+      <h3>New Recovered: {new Intl.NumberFormat().format(newRecovered)}</h3>
+    </div>
+  );
+};
+
+export default GlobalInfo;
