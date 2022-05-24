e80969c470483122f777edff4d15eb0014e460d5
damkols <damolausmankolapo@gmail.com>
2022-05-24 14:15:29 -0800

    chore: working on new features

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import type { ResponseData, Country } from "./components/types";
 const App: React.FunctionComponent = () => {
   const [data, setData] = useState<ResponseData | undefined>(undefined);
 
+  const [activeCountries, setActiveCountries] = useState<Country[]>([]);
+
   const fetchData = async () => {
     const result = await fetch("https://api.covid19api.com/summary");
     const data = await result.json();
@@ -19,9 +21,27 @@ const App: React.FunctionComponent = () => {
     fetchData();
   }, []);
 
+  const onCountryClick = (country: Country) => {
+    const countryIndex = activeCountries.findIndex(
+      (activeCountry) => activeCountry.ID
+    );
+
+    if (countryIndex > -1) {
+      const newActiveCountries = [...activeCountries];
+      newActiveCountries.splice(countryIndex, 1);
+
+      setActiveCountries(newActiveCountries);
+    } else {
+      setActiveCountries([...activeCountries, country]);
+    }
+  };
+
   return (
     <div className="App">
       <div className="global__info">
+        {activeCountries.map((activeCountry) => {
+          return <span>{activeCountry.Country}</span>;
+        })}
         {data ? (
           <>
             <GlobalInfo
@@ -29,7 +49,10 @@ const App: React.FunctionComponent = () => {
               newDeaths={data?.Global.NewDeaths}
               newRecovered={data?.Global.NewRecovered}
             />
-            <CountryList countries={data?.Countries} />
+            <CountryList
+              countries={data?.Countries}
+              onItemClick={onCountryClick}
+            />
           </>
         ) : (
           "Loading....."
